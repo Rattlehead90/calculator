05b983e9c5e8d1c704a3b7b0f1aabf4631d8c65c
Rattlehead90 <strogonov@seznam.cz>
2022-07-15 15:46:12 +0200

    Add operate() to handle the input

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+function operate(operator, a, b) {
+    if (operator === 'add') {
+        return add(a, b);
+    } else if (operator === 'subtract') {
+        return subtract(a, b);
+    } else if (operator === 'multiply') {
+        return multiply(a, b);
+    } else if (operator === 'divide') {
+        return divide(a, b);
+    } else {
+        return 'ERROR';
+    }
+}
