08af33dfffdf84f675f16765e40feb3e678c860d
Rattlehead90 <strogonov@seznam.cz>
2022-07-17 11:01:25 +0200

    Add keyboard support
    It's a first version, likely to be refactored into something more
    universal across the code (compatible with the functions for buttons).

--- script.js
+++ script.js
@@ -49,14 +49,20 @@ function clear() {
     buffer = null;
 }
 
-function operateOnBuffer(button) {
+function operateOnBuffer(key) { //there's a problem when you try to enter new number after equals
     if (operator) {
         display.textContent = operate(operator, buffer, Number(display.textContent));
-    }
-    operator = button.id;
+        }
+    operator = key;
     buffer = +display.textContent;
 }
 
+function equals() {
+    display.textContent = operate(operator, buffer, Number(display.textContent));
+    operator = null;
+}
+
+//Numpad functionality
 numpad.forEach((button) => {
     if (button.classList.contains('number')) {
         button.addEventListener('click', () => {
@@ -68,12 +74,46 @@ numpad.forEach((button) => {
 
     if (button.classList.contains('operator')) {
         button.addEventListener('click', () => {
-            operateOnBuffer(button);
+            operateOnBuffer(button.id);
         });
     }
 
-    if (button.id === 'equals') button.addEventListener('click', () => {
-        display.textContent = operate(operator, buffer, Number(display.textContent));
-        operator = null;
-    });
+    if (button.id === 'equals') button.addEventListener('click', equals);
 });
+
+//Keyboard support 
+
+function keyboardInput(e) {
+    if (isFinite(e.key)) {
+        if (display.textContent == buffer) {
+            display.textContent = '';
+        }
+        display.textContent += e.key;
+    }
+    
+    if (e.key === 'Enter') {
+        equals();
+    }
+//Operators from keyboard
+    if (e.key === '+') {
+        operateOnBuffer('add');
+    }
+
+    if (e.key === '-') {
+        operateOnBuffer('subtract');
+    } 
+    
+    if (e.key === '/') {
+        operateOnBuffer('divide');
+    }
+
+    if (e.key === '*') {
+        operateOnBuffer('multiply');
+    }
+
+    if (e.key === 'Backspace' || e.key === 'Delete') {
+        clear();
+    }
+}
+
+window.addEventListener('keydown', keyboardInput);
